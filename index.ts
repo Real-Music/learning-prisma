04ed9228df72056a0fa30a4b99d782110909538d
mr-ray <fedjioraymond@gmail.com>
2024-07-02 15:39:56 +0100

    Query: update

--- index.ts
+++ index.ts
@@ -3,19 +3,13 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-	// Create a new User
-	await prisma.user.create({
-		data: {
-			name: "Alice",
-			email: "alice@example.com",
-			posts: { create: { title: "Hello World" } },
-			profile: { create: { bio: "I love prisma" } }
-		}
-	});
+	// Update post
+	const post = await prisma.post.update({
+		where: { id: 1 },
+		data: { published: true }
+	})
 
-	// Read all Users and print them to the console
-	const allUsers = await prisma.user.findMany({ include: { posts: true, profile: true } });
-	console.dir(allUsers);
+	console.log(post)
 }
 
 main().then(async () => {
